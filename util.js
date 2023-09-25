1efafa18869d775020614e2fe69d43f71b90e8ad
Felipe Vieira Leal <felipevl205@gmail.com>
2023-09-25 18:29:07 -0300

    CorrecaoCarousel

--- util.js
+++ util.js
@@ -1,55 +1,37 @@
-// Função para avançar para o próximo item no carrossel
-function nextItem(carousel) {
-  const carouselContainer = carousel.querySelector('.carousel-container');
-  const carouselItems = carouselContainer.querySelector('.carousel-items');
-  const itemWidth = carouselItems.querySelector('li').offsetWidth;
+document.addEventListener("DOMContentLoaded", function () {
+  const carousel = document.querySelector(".carousel ul");
+  const items = document.querySelectorAll(".carousel ul li");
+  const prevButton = document.querySelector(".carousel-prev");
+  const nextButton = document.querySelector(".carousel-next");
+  const itemCount = items.length;
+  const itemsPerPage = 3; // Altere o número de itens exibidos por página aqui
+  let currentIndex = 0;
 
-  carouselItems.style.transform = `translateX(-${itemWidth}px)`;
+  // Função para avançar o carrossel
+  function nextSlide() {
+      currentIndex += itemsPerPage;
+      if (currentIndex >= itemCount) {
+          currentIndex = 0;
+      }
+      updateCarousel();
+  }
 
-  // Mover o primeiro item para o final da lista
-  carouselItems.appendChild(carouselItems.querySelector('li'));
-  carouselItems.style.transition = 'none';
+  // Função para voltar o carrossel
+  function prevSlide() {
+      currentIndex -= itemsPerPage;
+      if (currentIndex < 0) {
+          currentIndex = itemCount - itemsPerPage;
+      }
+      updateCarousel();
+  }
 
-  // Reiniciar a transformação para trazer o primeiro item de volta à vista
-  setTimeout(() => {
-    carouselItems.style.transition = '';
-    carouselItems.style.transform = 'translateX(0)';
-  }, 0);
-}
+  // Atualiza o estado do carrossel com base no índice atual
+  function updateCarousel() {
+      const translateX = -currentIndex * (100 / itemsPerPage);
+      carousel.style.transform = `translateX(${translateX}%)`;
+  }
 
-// Função para voltar para o item anterior no carrossel
-function prevItem(carousel) {
-  const carouselContainer = carousel.querySelector('.carousel-container');
-  const carouselItems = carouselContainer.querySelector('.carousel-items');
-  const itemWidth = carouselItems.querySelector('li').offsetWidth;
-
-  carouselItems.style.transition = 'none';
-
-  // Mover o último item para o início da lista
-  carouselItems.insertBefore(
-    carouselItems.querySelector('li:last-child'),
-    carouselItems.querySelector('li:first-child')
-  );
-
-  // Definir a transformação para a posição correta
-  carouselItems.style.transform = `translateX(-${itemWidth}px)`;
-
-  // Adicionar um pequeno atraso antes de aplicar a transição
-  setTimeout(() => {
-    carouselItems.style.transition = '';
-    carouselItems.style.transform = 'translateX(0)';
-  }, 0);
-}
-
-// Função para adicionar os ouvintes de evento aos botões de navegação
-function setupCarousel(carousel) {
-  const prevButton = carousel.querySelector('.carousel-prev');
-  const nextButton = carousel.querySelector('.carousel-next');
-
-  prevButton.addEventListener('click', () => prevItem(carousel));
-  nextButton.addEventListener('click', () => nextItem(carousel));
-}
-
-// Obter todos os elementos do carrossel e configurá-los
-const carousels = document.querySelectorAll('.carousel');
-carousels.forEach(setupCarousel);
+  // Adiciona os ouvintes de eventos para os botões de navegação
+  nextButton.addEventListener("click", nextSlide);
+  prevButton.addEventListener("click", prevSlide);
+});
